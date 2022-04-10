d601c7617add7627a21cef853dcfcf0078226503
Mohammad Abdur Rahman <id110067@connect.hku.hk>
2022-04-10 15:22:33 +0800

    Listening to additional ports

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.post("/work", function(req, res) {
   res.redirect("/work");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on Port 3000.")
 });
