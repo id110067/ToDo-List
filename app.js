86ca5e752f4bd5c231435af6f1d02871cec253d5
Mohammad Abdur Rahman <80828385+id110067@users.noreply.github.com>
2022-04-10 15:44:38 +0800

    deinitialise items array

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = [];
 const workItems = [];
 
 app.get("/", function(req, res) {
